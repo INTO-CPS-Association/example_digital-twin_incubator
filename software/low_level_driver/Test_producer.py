38e5f72fa9ea8b23492bdb05a207dd7b22ca5898
haof-github <59088379+haof-github@users.noreply.github.com>
2020-09-14 11:50:57 +0200

    Minor change

--- software/low_level_driver/Test_producer.py
+++ software/low_level_driver/Test_producer.py
@@ -1,5 +1,6 @@
 import pika
 import sys
+import json
 
 IP_raspberry = '10.17.98.141'
 IP_myLaptop =  '10.17.98.164'
@@ -16,12 +17,16 @@ connection = pika.BlockingConnection(parameters)
 # connection = pika.BlockingConnection(
 #     pika.ConnectionParameters(host='10.192.17.148',port=5672))
 channel = connection.channel()
-print("passing connection phase")
-channel.exchange_declare(exchange='topic_logs', exchange_type='topic')
+#print("passing connection phase")
+channel.exchange_declare(exchange='Incubator_AMQP', exchange_type='topic')
 
-routing_key = sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
-message = ' '.join(sys.argv[2:]) or 'Hello World!'
+routing_key = "incubator.hardware.w1.tempReading"#sys.argv[1] if len(sys.argv) > 2 else 'anonymous.info'
+message = {"Time":True,
+             "readTemperature1":True,
+             "readTemperature2":True,
+             "readTemperature3":True
+             }#' '.join(sys.argv[2:]) or 'Hello World!'
 channel.basic_publish(
-    exchange='topic_logs', routing_key=routing_key, body=message)
+    exchange='Incubator_AMQP', routing_key=routing_key, body=json.dumps(message))
 print(" [x] Sent %r:%r" % (routing_key, message))
 connection.close()
