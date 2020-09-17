263c7717f31ff29640d3b6bf13eab2999edde7b3
Claudio Gomes <cla.gms@gmail.com>
2020-09-17 16:40:45 +0200

    results of uniform temp tests

--- software/modelling/src/first_principles_model.py
+++ software/modelling/src/first_principles_model.py
@@ -3,7 +3,7 @@ from oomodelling import Model
 
 class IncubatorPlant(Model):
     def __init__(self, initial_heat_voltage=11.8, initial_heat_current=9.0, initial_room_temperature=25.0, initial_box_temperature=25.0,
-                 airC, boxG):
+                 C_air=1.0, G_box=1.0):
         super().__init__()
 
         self.in_heater_on = self.input(lambda: False)
@@ -13,22 +13,15 @@ class IncubatorPlant(Model):
 
         self.power_in = self.var(lambda: self.in_heater_voltage() * self.in_heater_current() if self.in_heater_on() else 0.0)
 
-        self.T = self.state()
+        self.G_box = self.parameter(G_box)
+        self.C_air = self.parameter(C_air)
 
-        self.power_out = self.var(lambda: self.)
-        self.der('in_qflow', lambda: self.in_heater_power())
+        self.T = self.state(initial_box_temperature)
 
-        self.box_air = ThermalCapacitor()
-        self.box_air.C = airC
+        self.power_out = self.var(lambda: self.G_box*(self.T() - self.in_room_temperature()))
 
-        self.box = ThermalConductor()
-        self.box.G = boxG
+        self.total_power = self.var(lambda: self.power_in() - self.power_out())
 
-        self.room_T = self.input(lambda: roomT)
-
-        self.box_air.QFlow = lambda: (self.in_qflow() - self.box.QFlow())
-
-        self.box.Ta = self.box_air.T
-        self.box.Tb = self.room_T
+        self.der('T', lambda: (1.0/self.C_air)*(self.total_power()))
 
         self.save()
--- software/modelling/test/first_principles_model.py
+++ software/modelling/test/first_principles_model.py
@@ -1,17 +1,10 @@
-import os
 import unittest
 
 import numpy
-import pandas as pandas
-import scipy
-from oomodelling.ModelSolver import ModelSolver
-from scipy.optimize import minimize
 
 from data_processing import load_data
-from src.functions import error, get_experiments, run_experiment
 import matplotlib.pyplot as plt
 
-from src.first_principles_model import IncubatorPlant, ComplexIncubatorPlant
 from scipy import integrate
 
 
@@ -19,7 +12,8 @@ class MyTestCase(unittest.TestCase):
 
     def test_plot_data(self):
         # CWD: H:\srcctrl\github\Example_Digital-Twin_Incubator\software\modelling\test
-        data = load_data("../../../datasets/calibration/uniform_temperature_heater_on_off.csv")
+        # data = load_data("../../../datasets/calibration/uniform_temperature_heater_on_off.csv")
+        data = load_data("../../digital_twin/diagnostics/output.csv")
 
         data["power_in"] = data.apply(lambda row: 11.8 * 10.45 if row.heater_on else 0.0, axis = 1)
 
@@ -62,78 +56,6 @@ class MyTestCase(unittest.TestCase):
 
         plt.show()
 
-    def test_calibrate(self):
-        params = [60,  # TON
-                  28,  # TOFF
-                  0.05,  # heatingG
-                  1.0,  # airC
-                  0.0005,  # boxG
-                  1e3]  # roomC
-        params = [6.44278926e+01, 4.84602587e+01, 6.89019487e-01, 6.03816268e+00,
-       2.86205065e+00, 1.03504504e+03]
-        print(minimize(error, params, method='BFGS', options={'maxiter': 1}))
-
-
-    def test_plot_experiments(self):
-        params = [87,  # TON
-                  29,  # TOFF
-                  0.032,  # heatingG
-                  1.0,  # airC
-                  0.00001,  # boxG
-                  1e3]  # roomC
-        params = [6.44278926e+01, 4.84602587e+01, 6.89019487e-01, 6.03816268e+00,
-                  2.86205065e+00, 1.03504504e+03]
-        tON = params[0]
-        tOFF = params[1]
-        experiments = get_experiments(tON, tOFF)
-        for i in range(len(experiments)):
-            exp = experiments[i]
-            m = run_experiment(exp, params, use_teval=False)
-            plt.figure()
-            plt.title(f"Experiment {i}")
-            plt.plot(m.signals['time'], [exp['T0'] for t in m.signals['time']], label="T0")
-            plt.plot(m.signals['time'], [exp['TF'] for t in m.signals['time']], label="TF")
-            plt.plot(m.signals['time'], m.box_air.signals['T'], label="T")
-            plt.plot(m.signals['time'], m.room_air.signals['T'], label="room_air T")
-            plt.legend()
-        plt.show()
-
-    def test_example_simulation(self):
-        def F(t, x):
-            return [-x[0]]
-        res = scipy.integrate.solve_ivp(F, (0, 10.0), [10.0])
-        plt.figure()
-        plt.plot(res.t, res.y[0, :], label="x")
-        plt.legend()
-        plt.show()
-
-    def test_do_prediction(self):
-        params = [6.44278926e+01, 4.84602587e+01, 6.89019487e-01, 6.03816268e+00,
-                  2.86205065e+00, 1.03504504e+03]
-        tON = params[0]
-        tOFF = params[1]
-        heatingG = params[2]
-        airC = params[3]
-        boxG = params[4]
-        roomC = params[5]
-        m = ComplexIncubatorPlant(tOFF, heatingG, airC, boxG, roomC)
-        m.box_air.T = 28.8
-        ModelSolver().simulate(m, 0.0, 4.0, 0.1)
-        plt.figure()
-        plt.plot(m.signals['time'], [28 for t in m.signals['time']], label="minT")
-        plt.plot(m.signals['time'], [33 for t in m.signals['time']], label="maxT")
-        plt.plot(m.signals['time'], m.box_air.signals['T'], label="T")
-        plt.legend()
-        plt.show()
-
-    def test_do_prediction(self):
-        m = IncubatorPlant(heatPow=100, roomT=0, airC=1, boxG=1.0)
-        m.box_air.T = 28.8
-        ModelSolver().simulate(m, 0.0, 10.0, 0.1)
-        plt.figure()
-        plt.plot(m.signals['time'], m.box_air.signals['T'], label="T")
-        plt.legend()
-        plt.show()
 
 
 if __name__ == '__main__':
