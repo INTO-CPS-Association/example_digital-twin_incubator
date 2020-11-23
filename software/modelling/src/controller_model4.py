9711a0693665ddd1c853a51b0b53d844b4f8e82d
haof.au@outlook.com <Freedom6264680>
2020-11-23 10:23:37 +0100

    restructure

--- software/modelling/src/controller_model4.py
+++ software/modelling/src/controller_model4.py
@@ -1,11 +1,18 @@
 from oomodelling import Model
-
+import time
 
 class ControllerModel4(Model):
-    def __init__(self, desired_temperature=45.0):
+    def __init__(self, desired_temperature=45.0, lower_bound=8, upper_bound=8, heating_time = 3, heating_gap = 1):
         super().__init__()
 
         self.T_desired = self.parameter(desired_temperature)
+
+        self.lower_bound = self.parameter(lower_bound)
+
+        self.upper_bound = self.parameter(upper_bound)
+
+        self.heating_time = self.parameter(heating_time)
+        self.heating_gap = self.parameter(heating_gap)
 
         self.in_temperature = self.input(lambda: 20.0)
 
@@ -23,9 +30,20 @@ class ControllerModel4(Model):
         """
         Returns whether heater is on or not (true or false)
         """
-        if T_desired >= in_temperature:
+        if in_temperature <= T_desired - self.lower_bound:
             self.cached_heater_on = True
+            time.sleep(self.heating_time)
+            self.cached_heater_on = False
+            time.sleep(self.heating_gap)
+            if in_temperature >= T_desired + self.upper_bound:
+                self.cached_heater_on = False
+            else:
+                self.cached_heater_on = True
         else:
             self.cached_heater_on = False
-
+            time.sleep(self.heating_gap)
+            if in_temperature >= T_desired + self.upper_bound:
+                self.cached_heater_on = False
+            else:
+                self.cached_heater_on = True
         return self.cached_heater_on
---
+++ software/old_digital_twin/experiments/Controller_Physical_Twin.py
@@ -0,0 +1,57 @@
+import logging
+import sys
+import time
+
+import pika
+import json
+import datetime
+from pika import BlockingConnection
+sys.path.append("../../communication/shared/")
+from connection_parameters import *
+from protocol import *
+
+
+class Controller_physical_twin():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
