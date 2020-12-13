314205f15013227afc97c86c0d7319b3a0b01060
Claudio Gomes <cla.gms@gmail.com>
2020-12-13 20:54:23 +0100

    slides draft done

--- software/digital_twin/models/plant_models/four_parameters_model/system_model4.py
+++ software/digital_twin/models/plant_models/four_parameters_model/system_model4.py
@@ -5,15 +5,20 @@ from digital_twin.models.plant_models.four_parameters_model.four_parameter_model
 
 
 class SystemModel4Parameters(Model):
-    def __init__(self):
+    def __init__(self, C_air,
+                 G_box,
+                 C_heater,
+                 G_heater, heating_time=0.1, heating_gap=1.0):
         super().__init__()
 
-        self.ctrl = ControllerModel4(desired_temperature=35)
-        self.plant = FourParameterIncubatorPlant()
+        self.ctrl = ControllerModel4(desired_temperature=35, heating_time=heating_time, heating_gap=heating_gap)
+        self.plant = FourParameterIncubatorPlant(initial_box_temperature=35.0,
+                                                 C_air=C_air,
+                                                 G_box=G_box,
+                                                 C_heater=C_heater,
+                                                 G_heater=G_heater)
 
         self.ctrl.in_temperature = self.plant.T
         self.plant.in_heater_on = self.ctrl.heater_on
 
         self.save()
-
-
--- software/tests/co_simulation/test_cosim_4_parameters.py
+++ software/tests/co_simulation/test_cosim_4_parameters.py
@@ -11,15 +11,32 @@ from tests.cli_mode_test import CLIModeTest
 class CosimulationTests(CLIModeTest):
 
     def test_run_cosim_4param_model(self):
-        m = SystemModel4Parameters()
-
-        ModelSolver().simulate(m, 0.0, 10, 0.01)
+        params = [486.1198196,  # C_air
+                  0.85804919,  # G_box
+                  33.65074598,  # C_heater
+                  0.86572258]  # G_heater
+        C_air_num = params[0]
+        G_box_num = params[1]
+        C_heater_num = params[2]
+        G_heater_num = params[3]
 
         plt.figure()
-        plt.plot(m.signals['time'], m.plant.signals['T'])
 
-        plt.figure()
-        plt.step(m.signals['time'], m.ctrl.signals['heater_on'])
+        def show_trace(heating_time):
+            m = SystemModel4Parameters(C_air=C_air_num,
+                                  G_box=G_box_num,
+                                  C_heater=C_heater_num,
+                                  G_heater=G_heater_num, heating_time=heating_time, heating_gap=1.0)
+            ModelSolver().simulate(m, 0.0, 1200, 0.5)
+
+            plt.plot(m.signals['time'], m.plant.signals['T'], label=f"Trial_{heating_time}")
+
+        show_trace(0.1)
+        show_trace(1.0)
+        show_trace(10.0)
+        show_trace(100.0)
+
+        plt.legend()
 
         if self.ide_mode():
             plt.show()
