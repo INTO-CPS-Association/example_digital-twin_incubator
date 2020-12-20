e067b7757207d685f7b9eae356a6ea84dee9f5ef
haof-github <59088379+haof-github@users.noreply.github.com>
2020-12-20 16:50:07 +0100

    changes for testing the fan.

--- software/old_software/old_digital_twin/experiments/test_fan_control.py
+++ software/old_software/old_digital_twin/experiments/test_fan_control.py
@@ -5,6 +5,21 @@ import json
 from communication.shared.connection_parameters import *
 from communication.shared.protocol import *
 
+RASPBERRY_IP = "10.17.98.239"
+RASPBERRY_PORT = 5672
+PIKA_USERNAME = "incubator"
+PIKA_PASSWORD = "incubator"
+PIKA_EXCHANGE = "Incubator_AMQP"
+PIKA_EXCHANGE_TYPE = "topic"
+PIKA_VHOST = "/"
+
+ROUTING_KEY_STATE = "incubator.driver.state"
+ROUTING_KEY_HEATER = "incubator.hardware.gpio.heater.on"
+ROUTING_KEY_FAN = "incubator.hardware.gpio.fan.on"
+ENCODING = "ascii"
+HEAT_CTRL_QUEUE = "heater_control"
+FAN_CTRL_QUEUE = "fan_control"
+
 
 if __name__ == '__main__':
     credentials = pika.PlainCredentials(PIKA_USERNAME, PIKA_PASSWORD)
