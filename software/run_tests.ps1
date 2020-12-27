a5ed5840667516a21d75abea7c890a18fa5db4d7
Claudio Gomes <cla.gms@gmail.com>
2020-12-27 08:36:55 +0100

    make script independent of virtual env

--- software/run_tests.ps1
+++ software/run_tests.ps1
@@ -1,10 +1,5 @@
-# Activate virtual environment
-& .\venv\Scripts\Activate.ps1
-
 # Set environment variable CLIMODE, so that tests know that they should not plot stuff.
 $Env:CLIMODE = "ON"
 
 # Run unittest in discovery mode for the tests folder
 & python -m unittest discover -v tests -p "*.py"
-
-pause
