ac9c73c5b9679b0f1e177abe3c6b9211323ddaba
Casper Thule <casper.thule@eng.au.dk>
2021-01-05 09:43:05 +0100

    added starting docker python script

---
+++ software/startup/start_docker_rabbitmq.py
@@ -0,0 +1,58 @@
+import subprocess
+import docker
+import requests
+import time
+import os
+
+dockerRelativeLocation = "../communication/installation"
+command = ["docker-compose", "up", "--detach", "--build"]
+containerName = "rabbitmq-server"
+sleepBetweenConnectionAttempts = 1
+maxConnectionAttempts = 10
+logFileName = "rabbitmq.log"
+
+
+def testConnection():
+    return requests.get("http://localhost:15672/api/overview", auth=('incubator', 'incubator'))
+
+
+def killContainer(containerName):
+    print("Searching for container with the name: " + containerName)
+    client = docker.from_env()
+    try:
+        container = client.containers.get(containerName)
+        if container.status == "running":
+            print("Container is running. Issuing kill request.")
+            container.kill()
+    except (docker.errors.NotFound, docker.errors.APIError) as x:
+        print("Exception in attempt to kill container: " + str(x))
+
+
+if __name__ == '__main__':
+    killContainer(containerName)
+
+    f = open(logFileName, "wt")
+    print("Running docker-compose command: " + " ".join(command))
+    print("Log will be stored in: " +os.path.abspath(logFileName))
+    proc = subprocess.run(command, cwd=dockerRelativeLocation, stdout=f)
+    if proc.returncode == 0:
+        print("docker-compose terminated succesfully.")
+    else:
+        print("docker-composed failed to terminate:" + str(proc.returncode))
+
+    rabbitmq_ready = False
+    attempts = maxConnectionAttempts
+    while rabbitmq_ready is False and attempts > 0:
+        try:
+            r = testConnection()
+            if r.status_code == 200:
+                print("RabbitMQ ready:\n " + r.text)
+                rabbitmq_ready = True
+        except requests.exceptions.ConnectionError as x:
+            print("RabbitMQ not ready - Exception: " + x.__class__.__name__)
+            time.sleep(sleepBetweenConnectionAttempts)
+            attempts -= 1
+
+    f.flush()
+    f.close()
+    killContainer(containerName)
