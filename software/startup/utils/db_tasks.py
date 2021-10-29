8322836841fd8d8857240fbb1882a95e8965bba8
Claudio Gomes <claudio.gomes@eng.au.dk>
2021-10-29 16:03:03 +0200

    Add checks for folder deleted.

--- software/startup/utils/db_tasks.py
+++ software/startup/utils/db_tasks.py
@@ -8,7 +8,8 @@ from incubator.config.config import resource_file_path
 def setup_db(influxdb_docker_dir="../../digital_twin/data_access/influxdbserver"):
     influx_db_folder_path = resource_file_path(influxdb_docker_dir)
     db_folder = os.path.join(influx_db_folder_path, "influxdb")
-    shutil.rmtree(os.path.join(influx_db_folder_path, "influxdb"), ignore_errors=True)
+    shutil.rmtree(db_folder, ignore_errors=True)
+    assert not os.path.exists(db_folder), f"Problem removing folder {db_folder}"
     os.mkdir(db_folder)
     with zipfile.ZipFile(os.path.join(influx_db_folder_path, "influxdb.zip"), 'r') as zip_ref:
         zip_ref.extractall(db_folder)
