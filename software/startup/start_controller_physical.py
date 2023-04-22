e920919b5320b9e889bfe74e5061dda76f04431b
Claudio Gomes <cla.gms@gmail.com>
2023-04-22 17:10:53 +0200

    Make controller more resilient to network drops.

--- software/startup/start_controller_physical.py
+++ software/startup/start_controller_physical.py
@@ -1,3 +1,5 @@
+import time
+
 from incubator.config.config import config_logger, load_config
 from incubator.physical_twin.controller_physical import ControllerPhysical
 
@@ -5,16 +7,22 @@ from incubator.physical_twin.controller_physical import ControllerPhysical
 def start_controller_physical(ok_queue=None):
     config_logger("logging.conf")
     config = load_config("startup.conf")
-    controller = ControllerPhysical(rabbit_config=config["rabbitmq"], **(config["physical_twin"]["controller"]))
-    controller.setup()
 
-    if ok_queue is not None:
-        ok_queue.put("OK")
-
-    controller.start_control()
+    while True:
+        try:
+            controller = ControllerPhysical(rabbit_config=config["rabbitmq"], **(config["physical_twin"]["controller"]))
+            controller.setup()
+            if ok_queue is not None:
+                ok_queue.put("OK")
+            controller.start_control()
+        except KeyboardInterrupt:
+            exit(0)
+        except Exception as exc:
+            print("Error: ")
+            print(exc)
+            print("Attempting to reconnect...")
+            time.sleep(1.0)
 
 
 if __name__ == '__main__':
     start_controller_physical()
-
-
