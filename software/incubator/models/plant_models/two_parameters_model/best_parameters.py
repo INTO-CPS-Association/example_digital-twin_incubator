08dd6ba340cc23e7049c3e2254e9a91ba5dda391
Claudio Gomes <cla.gms@gmail.com>
2023-04-23 08:08:48 +0200

    Better format file.

--- software/incubator/models/plant_models/two_parameters_model/best_parameters.py
+++ software/incubator/models/plant_models/two_parameters_model/best_parameters.py
@@ -1,2 +1,4 @@
-two_param_model_params = [616.56464029,  # C_air
-                          0.65001889]  # G_box
+two_param_model_params = [
+    616.56464029,   # C_air
+    0.65001889      # G_box
+]
