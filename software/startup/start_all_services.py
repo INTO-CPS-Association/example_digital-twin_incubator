ef9ec38f4667f519b489608aa1bf29ea18f73752
Claudio Gomes <cla.gms@gmail.com>
2023-04-24 05:36:18 +0200

    Restore start all services script to original form.

--- software/startup/start_all_services.py
+++ software/startup/start_all_services.py
@@ -14,23 +14,23 @@ from startup.start_supervisor import start_supervisor
 from startup.utils.start_as_daemon import start_as_daemon
 
 if __name__ == '__main__':
-    # start_docker_rabbitmq()
+    start_docker_rabbitmq()
     start_docker_influxdb()
 
-    # start_as_daemon(start_incubator_realtime_mockup)
-    # start_as_daemon(start_low_level_driver_mockup)
+    start_as_daemon(start_incubator_realtime_mockup)
+    start_as_daemon(start_low_level_driver_mockup)
     start_as_daemon(start_influx_data_recorder)
-    # start_as_daemon(start_plant_kalmanfilter)
+    start_as_daemon(start_plant_kalmanfilter)
 
-    # start_as_daemon(start_plant_simulator)
-    # start_as_daemon(start_simulator)
-    # start_as_daemon(start_calibrator)
+    start_as_daemon(start_plant_simulator)
+    start_as_daemon(start_simulator)
+    start_as_daemon(start_calibrator)
 
     # Choose one of the controllers below:
     # start_as_daemon(start_controller_physical)
-    # start_as_daemon(start_controller_physical_open_loop)
+    start_as_daemon(start_controller_physical_open_loop)
 
     # Enable self adaptation
-    # start_as_daemon(start_self_adaptation_manager)
-    # start_as_daemon(start_supervisor)
+    start_as_daemon(start_self_adaptation_manager)
+    start_as_daemon(start_supervisor)
 
