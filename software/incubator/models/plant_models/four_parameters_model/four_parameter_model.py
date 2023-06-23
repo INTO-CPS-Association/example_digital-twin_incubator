6b1b942f8f6a2a5e1e3a6ede0f38e8f6648a590a
Claudio Gomes <cla.gms@gmail.com>
2023-06-23 10:29:19 +0200

    Add a note on the differences between parameters of similar models.

--- software/incubator/models/plant_models/four_parameters_model/four_parameter_model.py
+++ software/incubator/models/plant_models/four_parameters_model/four_parameter_model.py
@@ -2,13 +2,21 @@ from incubator.models.plant_models.two_parameters_model.two_parameter_model impo
 
 
 class FourParameterIncubatorPlant(TwoParameterIncubatorPlant):
+    """
+    NOTE: This model inherits from the two parameter model so that we can reuse some of the equations
+    but the parameters, while having the same name, do not have the same meaning.
+    For example the C_air parameter has a very different meaning in this model
+    when compared to the C_air parameter in the two parameter model.
+    This is because the two parameter model is actually lumping the heater, air, and box, into the same entity,
+    whereas the four parameter model is just lumping the air and box, and contains a separate variable for the heater.
+    """
     def __init__(self,
                  initial_heat_voltage, initial_heat_current,
                  initial_room_temperature, initial_box_temperature,
                  initial_heat_temperature,
                  C_air, G_box,
                  C_heater, G_heater):
-        # Initialize 2p stuff
+        # Initialize 2p stuff.
         super(FourParameterIncubatorPlant, self).__init__(initial_heat_voltage, initial_heat_current,
                                                           initial_room_temperature, initial_box_temperature, C_air,
                                                           G_box)
